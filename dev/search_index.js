87237ec6b4a2b89aefde6665468ad46d526def68
Documenter.jl <documenter@juliadocs.github.io>
2024-12-07 18:56:26 +0000

    build based on 9a1f637

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PRIMME","category":"page"},{"location":"#PRIMME","page":"Home","title":"PRIMME","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PRIMME.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PRIMME]","category":"page"}]
+}
